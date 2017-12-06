3c5d31266331b26962b3550fdb10e4b0dd7f9c8b
Edmund Louie <leijeihui@gmail.com>
2017-12-06 00:32:24 -0800

    Convert to stateless component

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 import Chart from './Chart.jsx';
 
-class Landing extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div>
-        <Chart />
-      </div>
-
-    );
-  }
-}
+export const Landing = () => {
+  return (
+    <div>
+      <Chart />
+    </div>
+  );
+};
 
 export default Landing;
